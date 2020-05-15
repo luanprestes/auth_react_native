64b22bdf2cc655809f3e0ebc7c1b4b400f47799b
Luan Prestes <luan.prestes@tetris.co>
2020-05-14 21:12:37 -0300

    salvando login asyncStorage

--- AuthRN/src/Store/actions/users.ts
+++ AuthRN/src/Store/actions/users.ts
@@ -1,10 +1,13 @@
+import { AsyncStorage } from 'react-native';
 
 interface UserLogin{
   email: string, 
   password: string
 }
 
-export const authUser = (payload:UserLogin) => {
+export const authUser = async (payload:UserLogin) => {
+  await AsyncStorage.setItem('User', JSON.stringify(payload));
+
   return {
     type: "USER_AUTH",
     payload
--- AuthRN/src/routes/index.tsx
+++ AuthRN/src/routes/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AsyncStorage } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { isEmpty } from 'lodash';
 import { colorBase, colorText } from 'Config/assets';
 const Stack = createStackNavigator();
 
@@ -16,29 +18,49 @@ import Login from 'Screens/Login';
 import Home from 'Screens/Home';
 
 export default function Routes() {
+  const [user, setUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function handleAuth() {
+      const storageUser = await AsyncStorage.getItem('User');
+      setUser(storageUser);
+    }
+    
+    handleAuth();
+  })
+
   return (
     <Stack.Navigator 
-      initialRouteName="Welcome" 
+      initialRouteName={isEmpty(user) ? "Welcome" : "Home"}
       screenOptions={screenOptions} 
     > 
 
-      <Stack.Screen 
-        name="Welcome" 
-        component={Welcome} 
-        options={{ title: 'Bem Vindo' }} 
-      />
+      {isEmpty(user) ? 
+        <>
 
-      <Stack.Screen 
-        name="Home" 
-        component={Home} 
-        options={{ title: 'Home' }} 
-      />
+          <Stack.Screen 
+            name="Home" 
+            component={Home} 
+            options={{ title: 'Home' }} 
+          /> 
 
-      <Stack.Screen 
-        name="Login" 
-        component={Login} 
-        options={{ title: 'Fazer login' }} 
-      />
+        </> :
+        <>
+          <Stack.Screen 
+            name="Welcome" 
+            component={Welcome} 
+            options={{ title: 'Bem Vindo' }} 
+          />
+
+          <Stack.Screen 
+            name="Login" 
+            component={Login} 
+            options={{ title: 'Fazer login' }} 
+          />
+        </>
+
+      }
+
       
     </Stack.Navigator>
   );
